9a664d1998f9c96ca3da6f081ba4b0ee441ce383
alvaro aguirre <44936171+chelaschill@users.noreply.github.com>
2021-05-16 23:08:05 -0500

    Update Lab06Sec01VaccinationIsiftmoApplicationTests.java

--- src/test/java/cs/lab/lab06sec01vaccinationisiftmo/Lab06Sec01VaccinationIsiftmoApplicationTests.java
+++ src/test/java/cs/lab/lab06sec01vaccinationisiftmo/Lab06Sec01VaccinationIsiftmoApplicationTests.java
@@ -9,5 +9,26 @@ class Lab06Sec01VaccinationIsiftmoApplicationTests {
     @Test
     void contextLoads() {
     }
+    
+     @Test
+    void Test_length_dni(String dni) {
+        int largo = dni.length();
+        if(largo > 8){
+            Assert.hasLength(dni,"tiene mas de 8 caracteres.");
+        }
+        else{
+            Assert.hasLength(dni, "longitud correcta");
+        }
+    }
+
+    @Test
+    void Test_usuarios_max(int usuarios){
+        if(usuarios > 1000){
+            Assertions.assertEquals(1,2);
+        }
+        else{
+            Assertions.assertEquals(1, 1);
+        }
+    }
 
 }
